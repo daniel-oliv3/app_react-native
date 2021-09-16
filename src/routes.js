edbd9ae62b4538b0d70c96315cfff5da96bff55b
Daniel Oliveira <danielsapup3@gmail.com>
2021-09-16 09:54:11 -0300

    Home

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+import Home from './pages/Home';
 
 export default function Routes(){
+
+    const screenOptionsStyle = {
+        headerStyle: {
+            backgroundColor: '#121721'
+        },
+        headerTintColor: '#b02ab7' 
+    }
+
+
+
     return (
-        <>
-        <Text>Rotas SKATE PARADISE...</Text>
-        </>
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={screenOptionsStyle}>
+                <Stack.Screen 
+                name="Home" 
+                component={Home} 
+                options={{headerTitle: "Listar Orçamento"}}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
