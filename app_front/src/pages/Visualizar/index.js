c32b151d9c277fbe578efa58305cef2708d2e736
Daniel Oliveira <danielsapup3@gmail.com>
2021-09-18 11:36:01 -0300

    Update

--- app_front/src/pages/Visualizar/index.js
+++ app_front/src/pages/Visualizar/index.js
@@ -1,24 +1,29 @@
 import React, {useCallback, useState} from 'react';
-import { useFocusEffect } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+
 import { Alert, Text } from 'react-native';
+import { Container, TitleContent, DescContent } from './styles';
 
 import api from '../../config/configApi';
 
 export default function Visualizar({ route }){
 
     const [orcamento, setOrcamento] = useState('');
+
+    const navigation = useNavigation();
     
     const getOrcamento = async () => {
         try {
             const { orcamentoId } = route.params;
             const response = await api.get('/visualizar-orcamento/' + orcamentoId);
             setOrcamento(response.data.orcamento);
-            console.log(response.data.orcamento);
         }catch(err){
             if(err.response){
                 Alert.alert("", err.response.data.mensagem);
+                navigation.navigate('Home');
             }else{
                 Alert.alert("", "Erro: Nenhum orçamento encontrado, tente mais tarde!");
+                navigation.navigate('Home');
             }
         }
     }
@@ -26,13 +31,28 @@ export default function Visualizar({ route }){
     
     
     useFocusEffect(
-        useCallBack(() => {
+        useCallback(() => {
             getOrcamento();
         }, [])
     );
 
     return (
-        <Text>Visualizar Orçamento</Text>
+        <Container>
+            <TitleContent>ID</TitleContent>
+            <DescContent>{orcamento.id}</DescContent>
+
+            <TitleContent>Nome</TitleContent>
+            <DescContent>{orcamento.name}</DescContent>
+
+            <TitleContent>E-mail</TitleContent>
+            <DescContent>{orcamento.email}</DescContent>
+
+            <TitleContent>Assunto</TitleContent>
+            <DescContent>{orcamento.subject}</DescContent>
+
+            <TitleContent>Conteúdo</TitleContent>
+            <DescContent>{orcamento.content}</DescContent>
+        </Container>    
     )
 }
 
